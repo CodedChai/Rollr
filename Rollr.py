7caeccd81569cf9cfacbf3ecda2937ff9d846262
Skrillcon <guardianx515@gmail.com>
2015-10-12 15:33:45 -0500

    Implemented Spec 3 and 4.

--- Rollr.py
+++ Rollr.py
@@ -5,7 +5,17 @@ lineNumber = 0
 with open(sys.argv[1],'r') as jsCodeFile:
     for line in jsCodeFile:
         lineNumber+=1
-        if re.match("\s*$", line): continue
-        if not re.match(".+[\{\};][\s]*$", line): print("%d. Statement should end with a semicolon." % lineNumber)
-        if re.match(".*[ \t]$", line): print("%d. Statement should not have trailing whitespace." % lineNumber)
-    if not re.match(".*\n$", line): print("%d. File %s should end with a newline character." % (lineNumber, sys.argv[1]))
+        if re.match("\s*$", line):
+            continue
+        if not re.match(".+[\{\};][\s]*$", line):
+            print("%d. Statement should end with a semicolon." % lineNumber)
+        if re.match(".*[ \t]$", line):
+            print("%d. Statement should not have trailing whitespace." % lineNumber)
+        if re.match("\s*\{\s*", file_line):
+            print("%d. Open curly brace should not stand-alone." % line_num)
+        if re.search("\{.*\}", file_line):
+            print("%d. Closing curly brace should stand-alone." % line_num)
+        if re.search("(?<!=)==(?!=)", file_line):
+            print("%d. Should only use strict equality." % line_num)
+    if not re.match(".*\n$", line):
+        print("%d. File %s should end with a newline character." % (lineNumber, sys.argv[1]))
