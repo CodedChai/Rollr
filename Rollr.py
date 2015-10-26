a3fd30bfd9caf6e56e0b690c106c7f28df143271
Skrillcon <guardianx515@gmail.com>
2015-10-26 15:32:41 -0500

    Implemented all of the specs

--- Rollr.py
+++ Rollr.py
@@ -11,11 +11,18 @@ with open(sys.argv[1],'r') as jsCodeFile:
             print("%d. Statement should end with a semicolon." % lineNumber)
         if re.match(".*[ \t]$", line):
             print("%d. Statement should not have trailing whitespace." % lineNumber)
-        if re.match("\s*\{\s*", file_line):
-            print("%d. Open curly brace should not stand-alone." % line_num)
-        if re.search("\{.*\}", file_line):
-            print("%d. Closing curly brace should stand-alone." % line_num)
-        if re.search("(?<!=)==(?!=)", file_line):
-            print("%d. Should only use strict equality." % line_num)
+        if re.match("\s*\{\s*", line):
+            print("%d. Open curly brace should not stand-alone." % lineNumber)
+        if re.search("\{.*\}", line):
+            print("%d. Closing curly brace should stand-alone." % lineNumber)
+        if re.search("(?<!=)==(?!=)", line):
+            print("%d. Should only use strict equality." % lineNumber)
+        regex = re.search("(\".*\")", line)
+        if regex is not None and not re.search("'", regex.string[regex.start():regex.end()]):
+            print("%d. Should use single quotes." % lineNumber)
+        if re.search(";.*;", line):
+            print("%d. Use only one statement per line." % lineNumber)
+        if len(line) > 80:
+            print("%d. Lines should not be longer than 80 characters." % lineNumber)
     if not re.match(".*\n$", line):
         print("%d. File %s should end with a newline character." % (lineNumber, sys.argv[1]))
